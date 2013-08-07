31674720c2957931d932667e85873be2aae0a2e8
Marcin Zdun <mzdun@midnightbits.com>
2013-08-07 23:15:28 +0200

    Fix for http_response::clear and ostream << http_response_line

--- libnet/includes/http.hpp
+++ libnet/includes/http.hpp
@@ -53,6 +53,27 @@ namespace net
 			return o;
 		}
 
+		struct module_version
+		{
+			std::string m_name;
+			net::uint m_major;
+			net::uint m_minor;
+		};
+
+		inline std::ostream& operator << (std::ostream & o, const module_version& mod)
+		{
+			return o << mod.m_name << "/" << mod.m_major << "." << mod.m_minor;
+		}
+
+		module_version get_os_module_version();
+		module_version get_server_module_version();
+		inline module_version get_upnp_module_version() { return { "UPnP", 1, 1 }; }
+
+		inline std::tuple<module_version, module_version, module_version> get_server_version()
+		{
+			return std::make_tuple(get_os_module_version(), get_upnp_module_version(), get_server_module_version());
+		}
+
 		struct http_request_line
 		{
 			std::string m_method;
@@ -104,7 +125,47 @@ namespace net
 			o << first_line.m_protocol << " " << first_line.m_status << " ";
 			switch (first_line.m_status)
 			{
-			case 100: o << ""; break;
+			case 100: o << "Continue"; break;
+			case 101: o << "Switching Protocols"; break;
+			case 200: o << "OK"; break;
+			case 201: o << "Created"; break;
+			case 202: o << "Accepted"; break;
+			case 203: o << "Non-Authoritative Information"; break;
+			case 204: o << "No Content"; break;
+			case 205: o << "Reset Content"; break;
+			case 206: o << "Partial Content"; break;
+			case 300: o << "Multiple Choices"; break;
+			case 301: o << "Moved Permanently"; break;
+			case 302: o << "Found"; break;
+			case 303: o << "See Other"; break;
+			case 304: o << "Not Modified"; break;
+			case 305: o << "Use Proxy"; break;
+			case 306: o << "(Unused)"; break;
+			case 307: o << "Temporary Redirect"; break;
+			case 400: o << "Bad Request"; break;
+			case 401: o << "Unauthorized"; break;
+			case 402: o << "Payment Required"; break;
+			case 403: o << "Forbidden"; break;
+			case 404: o << "Not Found"; break;
+			case 405: o << "Method Not Allowed"; break;
+			case 406: o << "Not Acceptable"; break;
+			case 407: o << "Proxy Authentication Required"; break;
+			case 408: o << "Request Timeout"; break;
+			case 409: o << "Conflict"; break;
+			case 410: o << "Gone"; break;
+			case 411: o << "Length Required"; break;
+			case 412: o << "Precondition Failed"; break;
+			case 413: o << "Request Entity Too Large"; break;
+			case 414: o << "Request-URI Too Long"; break;
+			case 415: o << "Unsupported Media Type"; break;
+			case 416: o << "Requested Range Not Satisfiable"; break;
+			case 417: o << "Expectation Failed"; break;
+			case 500: o << "Internal Server Error"; break;
+			case 501: o << "Not Implemented"; break;
+			case 502: o << "Bad Gateway"; break;
+			case 503: o << "Service Unavailable"; break;
+			case 504: o << "Gateway Timeout"; break;
+			case 505: o << "HTTP Version Not Supported"; break;
 			}
 			return o << "\r\n";
 		}
@@ -120,35 +181,13 @@ namespace net
 				m_protocol = http_1_1;
 				m_status = 200;
 				mime::headers::clear();
-				append("server")->out() << http::get_server_module_version() << " (" << http::get_os_module_version() << ")";
+				append("server")->out() << get_server_module_version() << " (" << get_os_module_version() << ")";
 			}
 		};
 
 		inline std::ostream& operator << (std::ostream& o, const http_response& resp)
 		{
-			o << (const http_response_line&) resp << (const mime::headers&)resp << "\r\n";
-		}
-
-		struct module_version
-		{
-			std::string m_name;
-			net::uint m_major;
-			net::uint m_minor;
-		};
-
-		inline std::ostream& operator << (std::ostream & o, const module_version& mod)
-		{
-			return o << mod.m_name << "/" << mod.m_major << "." << mod.m_minor;
-		}
-
-		// to be implemented by a 
-		module_version get_os_module_version();
-		module_version get_server_module_version();
-		inline module_version get_upnp_module_version() { return { "UPnP", 1, 1 }; }
-
-		inline std::tuple<module_version, module_version, module_version> get_server_version()
-		{
-			return std::make_tuple(get_os_module_version(), get_upnp_module_version(), get_server_module_version());
+			return o << (const http_response_line&) resp << (const mime::headers&)resp << "\r\n";
 		}
 	}
 }
