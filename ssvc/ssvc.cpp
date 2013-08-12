f90f09d43bbdfd91b9d4165fcc96769a929a4782
Marcin Zdun <mzdun@midnightbits.com>
2013-08-12 13:58:48 +0200

    Crude IDL printing

--- ssvc/ssvc.cpp
+++ ssvc/ssvc.cpp
@@ -63,6 +63,52 @@ void help(Args&& ... args)
 
 inline bool is_file(const fs::path& p) { return fs::is_regular_file(p) || fs::is_symlink(p); }
 
+struct state_variable
+{
+	bool m_event;
+	std::string m_name;
+	std::string m_type;
+	std::vector<std::string> m_values;
+
+	std::string getType() const { return m_values.empty() ? m_type : m_event ? m_name + "_Values" : m_name; }
+};
+
+struct action_arg
+{
+	bool m_input;
+	std::string m_name;
+	std::string m_type_ref;
+	std::string getType(const std::vector<state_variable>& refs) const
+	{
+		for (auto&& ref : refs)
+		{
+			if (m_type_ref == ref.m_name)
+				return ref.getType();
+		}
+		return m_type_ref;
+	}
+};
+
+struct action
+{
+	std::string m_name;
+	std::vector<action_arg> m_args;
+};
+
+inline std::string find_string(const dom::XmlNodePtr& node, const std::string& xpath)
+{
+	auto tmp = node->find(xpath);
+	if (tmp) return tmp->stringValue();
+	return std::string();
+}
+
+inline std::string find_string(const dom::XmlNodePtr& node, const std::string& xpath, dom::Namespaces ns)
+{
+	auto tmp = node->find(xpath, ns);
+	if (tmp) return tmp->stringValue();
+	return std::string();
+}
+
 int main(int argc, char* argv [])
 {
 	try
@@ -93,7 +139,115 @@ int main(int argc, char* argv [])
 		auto doc = dom::XmlDocument::fromDataSource([&](void* buffer, size_t size) { return (size_t)in_f.read((char*)buffer, size).gcount(); });
 		if (doc)
 		{
+			std::vector<state_variable> types_variables;
+			std::vector<action> actions;
+			int spec_major = 0, spec_minor = 0;
+
 			// try XML -> IDL
+			dom::NSData ns[] = { {"svc", "urn:schemas-upnp-org:service-1-0"} };
+			auto version = doc->find("/svc:scpd/svc:specVersion", ns);
+			if (version)
+			{
+				auto major = find_string(version, "svc:major", ns);
+				auto minor = find_string(version, "svc:minor", ns);
+				{
+					std::istringstream i(major);
+					i >> spec_major;
+				}
+				{
+					std::istringstream i(minor);
+					i >> spec_minor;
+				}
+			}
+
+			auto action_list = doc->findall("/svc:scpd/svc:actionList/svc:action", ns);
+			for (auto&& act : action_list)
+			{
+				action action;
+				action.m_name = find_string(act, "svc:name", ns);
+
+				auto args = act->findall("svc:argumentList/svc:argument", ns);
+				for (auto&& arg : args)
+				{
+					action_arg action_arg;
+					action_arg.m_input = find_string(arg, "svc:direction", ns) == "out";
+					action_arg.m_name = find_string(arg, "svc:name", ns);
+					action_arg.m_type_ref = find_string(arg, "svc:relatedStateVariable", ns);
+					action.m_args.push_back(action_arg);
+				}
+				actions.push_back(action);
+			}
+
+			auto variables = doc->findall("/svc:scpd/svc:serviceStateTable/svc:stateVariable", ns);
+			for (auto&& variable : variables)
+			{
+				auto sendEvent = find_string(variable, "@sendEvents");
+
+				state_variable var;
+				var.m_event = sendEvent == "1" || sendEvent == "yes";
+				var.m_name = find_string(variable, "svc:name", ns);
+				var.m_type = find_string(variable, "svc:dataType", ns);
+
+				auto allowedValues = variable->findall("svc:allowedValueList/svc:allowedValue", ns);
+				for (auto&& value : allowedValues)
+					var.m_values.push_back(value->stringValue());
+
+				types_variables.push_back(var);
+			}
+
+			size_t events = 0;
+			for (auto&& var : types_variables)
+			{
+				if (var.m_values.empty())
+				{
+					++events;
+					continue;
+				}
+
+				std::cout << "enum " << var.getType() << " { // " << var.m_type << "\n";
+				for (auto&& val : var.m_values)
+				{
+					std::cout << "    " << val << ";\n";
+				}
+				std::cout << "};\n\n";
+			}
+
+			std::cout << "[version(" << spec_major << "." << spec_minor << ")] interface <name missing> {\n";
+
+			for (auto&& action : actions)
+			{
+				bool first = true;
+				std::cout << "    void " << action.m_name << "(";
+
+				size_t  out_count = 0;
+				for (auto&& arg : action.m_args)
+				{
+					if (!arg.m_input)
+						++out_count;
+				}
+
+				for (auto&& arg : action.m_args)
+				{
+					if (first) first = false;
+					else std::cout << ", ";
+					std::cout << (arg.m_input ? "[in] " : out_count == 1 ? "[retval] " : "[out] ") << arg.getType(types_variables) << " " << arg.m_name;
+				}
+				std::cout << ");\n";
+			}
+
+			if (!types_variables.empty() && events > 0)
+				std::cout << "\n";
+
+			for (auto && var : types_variables)
+			{
+				if (!var.m_event)
+					continue;
+
+				std::cout << "    ";
+				std::cout << var.getType() << " " << var.m_name << ";\n";
+			}
+
+			std::cout << "};\n";
 		}
 		else
 		{
